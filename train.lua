a3bd7447b5ff12e8dfeb13081b8011b1b7459338
xshen99 <xshen5@buffalo.edu>
2018-04-08 22:54:12 -0400

    back to decay

--- train.lua
+++ train.lua
@@ -238,7 +238,7 @@ function Trainer:learningRate(epoch)
    local decay = 0
    if self.opt.dataset == 'imagenet' then
       decay = math.floor((epoch - 1) / 30)
-  elseif self.opt.dataset == 'cifar10' then --or self.opt.dataset =='lungroi' then
+  elseif self.opt.dataset == 'cifar10' or self.opt.dataset =='lungroi' then
       decay = epoch >= 0.75*self.opt.nEpochs and 2 or epoch >= 0.5*self.opt.nEpochs and 1 or 0
    elseif self.opt.dataset == 'cifar100' then
       decay = epoch >= 0.75*self.opt.nEpochs and 2 or epoch >= 0.5*self.opt.nEpochs and 1 or 0
